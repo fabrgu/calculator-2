96cd5436cb6eaef5647cad5a9012e9c3c1dfb03c
Your Name <you@example.com>
2020-01-29 11:43:26 -0800

    implemented a REPL

--- calculator.py
+++ calculator.py
@@ -4,7 +4,37 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
-
+import arithmetic
 
 # Your code goes here
+while True:
+    input_str = input("Enter your calculations: ")
+    tokens = input_str.split(" ")
+    first_token = tokens[0]
+    if first_token.lower() == "q":
+        break
+    else:
+        answer = ""
+        second_token = float(tokens[1])
+        third_token = None
+        if len(tokens) == 3:
+            third_token = float(tokens[2])
+
+        if first_token == "+":
+            answer = arithmetic.add(second_token, third_token)
+        elif first_token == "-":
+            answer = arithmetic.subtract(second_token, third_token)
+        elif first_token == "*":
+            answer = arithmetic.multiply(second_token, third_token)
+        elif first_token == "/":
+            answer = arithmetic.divide(second_token, third_token)
+        elif first_token == "square":
+            answer = arithmetic.square(second_token)
+        elif first_token == "cube":
+            answer = arithmetic.cube(second_token)
+        elif first_token == "pow":
+            answer = arithmetic.power(second_token, third_token)
+        elif first_token == "mod":
+            answer = arithmetic.mod(second_token, third_token)
+
+        print(answer)
